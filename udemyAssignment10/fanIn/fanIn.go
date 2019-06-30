423edbdf4b48e6586bab43d56e3670c5dcfbec3c
Pat Alwell <pat.alwell@gmail.com>
2019-06-30 16:11:30 -0700

    still in work on assignment10. finishing concurrency patterns.

--- udemyAssignment10/fanIn/fanIn.go
+++ udemyAssignment10/fanIn/fanIn.go
@@ -1,5 +1,60 @@
 package main
 
-func main() {
-	
+import (
+	"fmt"
+	"sync"
+)
+
+// send channel
+func send(even, odd chan<- int) {
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			even <- i
+		} else {
+			odd <- i
+		}
+	}
+	close(even)
+	close(odd)
 }
+
+// Iterate over the even and off channels and fan them into a receiver channel fanin
+func receive(even, odd <-chan int, fanin chan<- int) {
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		for v := range even {
+			fanin <- v
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		for v := range odd {
+			fanin <- v
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
+	close(fanin)
+}
+
+func main() {
+	even := make(chan int)
+	odd := make(chan int)
+	fanin := make(chan int)
+
+	go send(even, odd)
+
+	go receive(even, odd, fanin)
+
+	for v := range fanin {
+		fmt.Println(v)
+	}
+
+	fmt.Println("about to exit")
+}
+
+
