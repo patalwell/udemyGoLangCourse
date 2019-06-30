0b47665b664a30214a8d5d4f58c882425dbdb1f6
Pat Alwell <pat.alwell@gmail.com>
2019-06-29 21:55:39 -0700

    working on wait groups

--- udemyAssignment9/udemyAssignment9.go
+++ udemyAssignment9/udemyAssignment9.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+//Exercise 1:
+//in addition to the main goroutine, launch two additional goroutines
+//each additional goroutine should print something out
+func routineOne() {
+	fmt.Println("goRoutineOne")
+}
+func routineTwo(){
+	fmt.Println("goRoutineTwo")
+}
+//use waitgroups to make sure each goroutine finishes before your program exists
+
+var waitGroup sync.WaitGroup
 func main() {
+
+	//Exercise 1:
+	//Add two routines to the waitGroup
+	waitGroup.Add(2)
+	go func() {
+		routineOne()
+		waitGroup.Done()
+		fmt.Println("GoRoutines: ", runtime.NumGoroutine())
+	}()
+
+	go func() {
+		routineTwo()
+		waitGroup.Done()
+		fmt.Println("GoRoutines: ", runtime.NumGoroutine())
+	}()
+
+	//Wait till our routines run prior to exiting main()
+	waitGroup.Wait()
+	fmt.Println("GoRoutines: ", runtime.NumGoroutine())
 	
 }
